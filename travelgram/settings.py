aebf15f5b572485b9385e33473f1c07c23e3c644
jcmunozo <jcmunozo@unicauca.edu.co>
2024-07-08 00:47:35 -0500

    Added logoutView

--- travelgram/settings.py
+++ travelgram/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from dotenv import load_dotenv
+import os
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your_key'
+SECRET_KEY = os.getenv('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 """Users views"""
 #django
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views as auth_views
 from django.views.generic import DetailView, FormView, UpdateView
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import render
 
 #forms
 from users.forms import SignupForm
@@ -63,5 +63,12 @@ class LoginView(auth_views.LoginView):
     redirect_authenticated_user = True
 
 class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
-    """Logout view"""
-    pass
+    """Logout view""" 
+    template_name = 'users/logout.html'  # Página de confirmación
+    next_page = reverse_lazy('users:login')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            return render(request, self.template_name)
+        elif request.method == 'POST':
+            return super().dispatch(request, *args, **kwargs)
